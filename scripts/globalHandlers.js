9b92c04d7b6bb1dc42819781768ed8e97ec69a05
Dioguerreiro <diogo.guerreiro8@hotmail.com>
2023-11-15 19:40:20 +0000

    fix esLint errors on scripts folder

--- scripts/globalHandlers.js
+++ scripts/globalHandlers.js
@@ -1,6 +1,6 @@
 "use strict";
-const mysql = require("mysql2");
-const options = require("./connection-options.json");
+// const mysql = require("mysql2");
+// const options = require("./connection-options.json");
 
 
 //============================================================= Logout
